6755bf39805856341f7f8052699f9b3c1bbb1f07
nmanumr <nmanumr@gmail.com>
2021-07-23 21:34:30 +0500

    update

--- demos/deno/index.ts
+++ demos/deno/index.ts
@@ -1,10 +1,8 @@
-// to run: deno run --import-map=demos/deno/import_map.json demos/deno/index.ts
-
 import * as Rectangular  from "mazes101/boards/rectangular.ts";
 import {generate} from 'mazes101/generators/backtrack.ts';
 import {render} from 'mazes101/renderers/rectangularSvg.ts';
 
-let board = Rectangular.newBoard({height: 10, width: 10});
+let board = Rectangular.newBoard({height: 7, width: 7});
 board = generate(board, Rectangular);
 
 let svg = render(board);
