0ebf798ed261f62e78d68050648bd0fdcc8393da
nmanumr <nmanumr@gmail.com>
2021-07-27 13:47:07 +0500

    feat(renderers): adds support for path rendering

--- src/renderers/circularSvg.ts
+++ src/renderers/circularSvg.ts
@@ -1,21 +1,27 @@
 import {CircularBoard, Direction, getRows} from "../boards/circular";
 import {BoardType} from "../base";
 import {StrH as globalH} from '../h';
+import {fillColor, findCellPathId} from "./utils";
 
 interface RendererOptions<T> {
   cellSize: number;
   lineWidth: number;
+  paths: Record<number | string, number[]>,
+  colors: Record<number | string, string>,
   h: (tag: string, attributes: Record<string, string>, ...children: Array<any>) => T;
 }
 
 const defaultOptions: RendererOptions<string> = {
   cellSize: 30,
   lineWidth: 2,
+  paths: {},
+  colors: {},
   h: globalH,
 }
 
 export function render<T = string>(board: CircularBoard, options: Partial<RendererOptions<T>> = {}): T {
   let opts: RendererOptions<T> = {...defaultOptions, ...options} as RendererOptions<T>;
+  let colors = fillColor(opts.paths, opts.colors);
 
   const innerRadius = board.size.innerRadius;
   const radiusOffset = (1 - innerRadius) * opts.cellSize * 0.75;
@@ -25,11 +31,15 @@ export function render<T = string>(board: CircularBoard, options: Partial<Render
   const center = canvasSize / 2;
 
   let rows = getRows(board);
-  let path = '';
+  let paths: Record<string, string> = {};
+  let walls = '';
 
   for (let r = innerRadius; r < rows.length + innerRadius; r++) {
     for (let i = 0; i < rows[r - innerRadius].length; i++) {
-      const cell = board.cells[rows[r - innerRadius][i]];
+      const cellIndex = rows[r - innerRadius][i];
+      const cell = board.cells[cellIndex];
+
+      const pathId = findCellPathId(cellIndex, opts.paths);
 
       const cellArc = 2 * Math.PI / rows[r - innerRadius].length;
       const innerArcRadius = r * opts.cellSize + radiusOffset;
@@ -44,17 +54,21 @@ export function render<T = string>(board: CircularBoard, options: Partial<Render
         .map(t => [Math.cos(t), Math.sin(t)].map((i) => center + outerArcRadius * i));
 
       if ((cell & Direction.BOTTOM) === 0) {
-        path += `M${xi1},${yi1}A${innerArcRadius},${innerArcRadius},0,0,1,${xi2},${yi2}`;
+        walls += `M${xi1},${yi1}A${innerArcRadius},${innerArcRadius},0,0,1,${xi2},${yi2}`;
       }
       if ((cell & Direction.LEFT) === 0) {
-        path += `M${xi1},${yi1}L${xo1},${yo1}`;
+        walls += `M${xi1},${yi1}L${xo1},${yo1}`;
       }
       if ((cell & Direction.RIGHT) === 0) {
-        path += `M${xi2},${yi2}L${xo2},${yo2}`;
+        walls += `M${xi2},${yi2}L${xo2},${yo2}`;
+      }
+      if (r === (rows.length + innerRadius) - 1) {
+        walls += `M${xo1},${yo1}A${outerArcRadius},${outerArcRadius},0,0,1,${xo2},${yo2}`
       }
 
-      if (r === (rows.length + innerRadius) - 1) {
-        path += `M${xo1},${yo1}A${outerArcRadius},${outerArcRadius},0,0,1,${xo2},${yo2}`
+      if (pathId) {
+        let closedPath = `M${xi1},${yi1}A${innerArcRadius},${innerArcRadius},0,0,1,${xi2},${yi2}L${xo2},${yo2}A${outerArcRadius},${outerArcRadius},0,0,0,${xo1},${yo1}z`;
+        paths[pathId] = ((paths[pathId] || '') + closedPath);
       }
     }
   }
@@ -69,7 +83,10 @@ export function render<T = string>(board: CircularBoard, options: Partial<Render
       height: `${canvasSize}px`,
       viewBox: `0 0 ${canvasSize} ${canvasSize}`
     },
-    h("path", {d: path, strokeWidth: `${opts.lineWidth}px`, strokeLinecap: "round"})
+    Object.entries(paths).map(([k, path]) => {
+      return h('path', {d: path, fill: colors[k], key: k, strokeWidth: `0`});
+    }),
+    h("path", {d: walls, strokeWidth: `${opts.lineWidth}px`, strokeLinecap: "round"})
   );
 }
 
--- src/renderers/rectangularSvg.ts
+++ src/renderers/rectangularSvg.ts
@@ -1,53 +1,57 @@
 import {Direction, RectangularBoard, toPosition} from "../boards/rectangular";
-import {BoardType, isEnabled} from "../base";
+import {BoardType} from "../base";
 import {StrH as globalH} from "../h";
+import {fillColor, findCellPathId, getColors} from "./utils";
 
 interface RendererOptions<T> {
   cellSize: number;
   lineWidth: number;
-  renderDisabled: boolean;
+  paths: Record<number | string, number[]>,
+  colors: Record<number | string, string>,
   h: (tag: string, attributes: Record<string, string>, ...children: Array<any>) => T;
 }
 
 const defaultOptions: RendererOptions<string> = {
   cellSize: 30,
   lineWidth: 2,
-  renderDisabled: false,
+  paths: {},
+  colors: {},
   h: globalH,
 }
 
 export function render<T = string>(board: RectangularBoard, options: Partial<RendererOptions<T>> = {}): T {
   let opts: RendererOptions<T> = {...defaultOptions, ...options} as RendererOptions<T>;
+  let colors = fillColor(opts.paths, opts.colors);
 
   const width = opts.cellSize * (board.size.width + 2) + opts.lineWidth;
   const height = opts.cellSize * (board.size.height + 2) + opts.lineWidth;
-  let p2 = "";
+  let paths: Record<string, string> = {};
+  let walls = '';
 
-  const path = board.cells.reduce((acc, cell, index) => {
+  board.cells.forEach((cell, index) => {
     const {x, y} = toPosition(index, {size: board.size});
     const pivotX = x * opts.cellSize + (opts.lineWidth / 2) + opts.cellSize;
     const pivotY = y * opts.cellSize + (opts.lineWidth / 2) + opts.cellSize;
-
-    if (!isEnabled(cell)) {
-      p2 += `M${pivotX},${pivotY}H${pivotX + opts.cellSize}V${pivotY + opts.cellSize}H${pivotX}z`;
-      return acc;
-    }
+    const pathId = findCellPathId(index, opts.paths);
 
     if ((cell & Direction.TOP) === 0) {
-      acc += `M${pivotX},${pivotY}H${pivotX + opts.cellSize}`;
+      walls += `M${pivotX},${pivotY}H${pivotX + opts.cellSize}`;
     }
     if ((cell & Direction.RIGHT) === 0) {
-      acc += `M${pivotX + opts.cellSize},${pivotY}V${pivotY + opts.cellSize}`;
+      walls += `M${pivotX + opts.cellSize},${pivotY}V${pivotY + opts.cellSize}`;
     }
     if ((cell & Direction.BOTTOM) === 0) {
-      acc += `M${pivotX},${pivotY + opts.cellSize}H${pivotX + opts.cellSize}`;
+      walls += `M${pivotX},${pivotY + opts.cellSize}H${pivotX + opts.cellSize}`;
     }
     if ((cell & Direction.LEFT) === 0) {
-      acc += `M${pivotX},${pivotY}V${pivotY + opts.cellSize}`;
+      walls += `M${pivotX},${pivotY}V${pivotY + opts.cellSize}`;
     }
 
-    return acc;
-  }, '');
+    if (pathId) {
+      let closedPath = `M${pivotX},${pivotY}H${pivotX + opts.cellSize}V${pivotY + opts.cellSize}H${pivotX}z`;
+      paths[pathId] = ((paths[pathId] || '') + closedPath);
+    }
+  });
 
   const h = opts.h;
   return h(
@@ -59,8 +63,10 @@ export function render<T = string>(board: RectangularBoard, options: Partial<Ren
       height: `${height}px`,
       viewBox: `0 0 ${width} ${height}`
     },
-    opts.renderDisabled && h('path', {d: p2, className: 'disabledPath'}),
-    h('path', {d: path, strokeWidth: `${opts.lineWidth}px`, strokeLinecap:'round'})
+    Object.entries(paths).map(([k, path]) => {
+      return h('path', {d: path, fill: colors[k], key: k, strokeWidth: `0`});
+    }),
+    h('path', {d: walls, strokeWidth: `${opts.lineWidth}px`, strokeLinecap: 'round'}),
   );
 }
 
